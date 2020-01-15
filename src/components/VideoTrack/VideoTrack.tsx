a3e7ba88323861466beb366f426c76e9c2dcacf6
Tim Mendoza <tmendoza@twilio.com>
2020-01-15 15:45:16 -0700

    Improve videoTrack component

--- src/components/VideoTrack/VideoTrack.tsx
+++ src/components/VideoTrack/VideoTrack.tsx
@@ -11,44 +11,38 @@ interface VideoTrackProps {
   videoEl?: HTMLVideoElement;
 }
 
-function updateStream(stream: MediaStream, track: MediaStreamTrack) {
-  const tracks = stream.getTracks();
-  if (tracks.includes(track)) {
-    return;
-  }
-  tracks.forEach(track => stream.removeTrack(track));
-  stream.addTrack(track);
-}
-
-export default function VideoTrack({
-  track,
-  isLocal,
-  priority,
-  videoEl = document.createElement('video'),
-}: VideoTrackProps) {
+export default function VideoTrack({ track, isLocal, priority, videoEl }: VideoTrackProps) {
   const containerRef = useRef<HTMLDivElement>(null!);
-  const streamRef = useRef(new MediaStream());
+  const videoElRef = useRef(videoEl);
 
   useEffect(() => {
-    containerRef.current.appendChild(videoEl);
-    // When a value is assigned to the srcObject property, it causes a brief flicker in video elements that are already
-    // displaying video. To prevent this, we assign a value to srcObject only once. If this component is rendered
-    // with a new video track, we update the video element by replacing the MediaStreamTrack in the MediaStream.
-    // This allows us to display a new participant without a flicker in the video. See 'updateStream' function.
-    videoEl.srcObject = streamRef.current;
-    videoEl.muted = true;
-    videoEl.autoplay = true;
-    return () => {
-      containerRef.current.removeChild(videoEl);
-    };
-  }, [videoEl]);
+    if (!videoElRef.current) {
+      // Create a video element if it wasn't passed as a prop
+      videoElRef.current = document.createElement('video');
+    }
+    const el = videoElRef.current;
+    containerRef.current.appendChild(el);
+    if (!el.srcObject) {
+      // When a value is assigned to the srcObject property, it causes a brief flicker in video elements that are already
+      // displaying video. To prevent this, we assign a value to srcObject only once. If this component is rendered
+      // with a new video track, we update the video element by replacing the MediaStreamTrack in the MediaStream.
+      // This allows us to display a new participant without a flicker in the video.
+      el.srcObject = new MediaStream();
+    }
+    el.muted = true;
+    el.autoplay = true;
+  }, []);
 
   useEffect(() => {
     if (track.setPriority && priority) {
       track.setPriority(priority);
     }
-    updateStream(streamRef.current, track.mediaStreamTrack);
+
+    const stream = videoElRef.current!.srcObject as MediaStream;
+    stream.addTrack(track.mediaStreamTrack);
+
     return () => {
+      stream.removeTrack(track.mediaStreamTrack);
       if (track.setPriority && priority) {
         // Passing `null` to setPriority will set the track's priority to that which it was published with.
         track.setPriority(null);
